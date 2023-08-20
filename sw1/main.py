cdc0bb59c7161345732ac26d11c44646546fa30f
loadthecode0 <artyaastha@gmail.com>
2023-08-20 19:50:22 +0530

    Writing done

--- sw1/main.py
+++ sw1/main.py
@@ -5,23 +5,14 @@ class Node :
         self.nodeName = name
         self.is_input = if_input
         self.is_output = if_output
-        self.gate = [] # gate(s) possibly outputting to this node
+        self.gate = 0 # gate outputting to this node
         self.outGates = []
         self.inputs = [] 
-        self.outputs = [] 
         self.inDelay = 0 #for part A
         self.reqDelay = 0 #for Part B
         
-    #custom priority comparator
-    # def __lt__(self, otherNode) -> bool:
-    #     output = False
-    #     if (self.nodeName in otherNode.inputs):
-    #         output = True
-    #     print(self.nodeName, otherNode.nodeName, self.nodeName in otherNode.inputs)
-    #     return (output)
-        
     def displayNode(self):
-        print(self.nodeIndex, self.nodeName, self.is_input, self.is_output, self.gate, self.outGates, self.inDelay, self.inputs, self.outputs, self.reqDelay)
+        print(self.nodeIndex, self.nodeName, self.is_input, self.is_output, self.gate, self.outGates, self.inDelay, self.inputs, self.reqDelay)
         
 class Gate :
     def __init__(self, index, type, inputs, output) -> None:
@@ -30,11 +21,6 @@ class Gate :
         self.gateDelay = 0 #delay at time of output
         self.inputs = inputs #list of nodes giving input
         self.output = output #Node taking output
-        
-    # def calculateGateOutputDelay(self, circuit) -> int:
-    #     t = 0
-    #     for x in range(len(self.inputs)):
-    #         t = max(t, circuit.inputNode[self.input[x]])
             
     def displayGate(self):
         print (self.gateIndex, self.gateType, self.gateDelay, self.inputs, self.output)
@@ -45,24 +31,17 @@ class Circuit:
         
         self.gateCount = 0 
         self.nodeCount = 0
-        
-        # dictionaries of delays, delete if not required
-        self.intNodesDelays = {}
-        self.inputNodesDelays = {}
-        self.outputNodesDelays = {}
-        
+                
         #dictionary of node objects, accessed by their names
         self.nodes = {}
-        self.nodesArray = [] 
-        self.inputNodesList = []
-        self.intNodesList = []
+        
+        #lists of node objects
+        self.inputNodesList = [] 
         self.outputNodesList = []
         
         #useful stuff, don't mess around with these
         self.gates= [] #list containing all gate OBJECTS
         self.gateTypeDict = {}
-        # self.gateInputNodes=[]
-        # self.gateOutputNodes=[]
         
     def nodeInserter(self,arr, if_input, if_output) :
         for z in range(len(arr)) :
@@ -75,9 +54,6 @@ class Circuit:
                 self.inputNodesList.append(self.nodes[name])
             elif (if_output == True) :
                 self.outputNodesList.append(self.nodes[name])
-            else :
-                self.intNodesList.append(self.nodes[name])
-            # self.nodes[name].displayNode()
             
     def displayCircuit(self) :
         for i in self.nodes:
@@ -85,7 +61,6 @@ class Circuit:
         for j in range(len(self.gates)) :
             self.gates[j].displayGate()
             
-
 
 #reading inputs and updating created objects ============================================================
 
@@ -106,12 +81,6 @@ def readCircuitFile(circuit, filename) :
         if (wordsInLine[0]=='PRIMARY_INPUTS' ):
             circuit.inputNodesDelays = dict.fromkeys(wordsInLine[1:], 0)
             circuit.nodeInserter(wordsInLine[1:], True, False) #inserts all nodes in current line
-            # for z in range(len(wordsInLine[1:])) :
-            #     circuit.nodeCount += 1
-            #     index = circuit.nodeCount
-            #     name = wordsInLine[1:][z]
-            #     circuit.nodes.append(Node(index, name, True, False))
-            #     circuit.nodes[index-1].displayNode()
         elif (wordsInLine[0]=='PRIMARY_OUTPUTS'):
             circuit.outputNodesDelays = dict.fromkeys(wordsInLine[1:], 0)
             circuit.nodeInserter(wordsInLine[1:], False, True)
@@ -191,20 +160,15 @@ def readReqDelayFile (circuit, filename) :
         if ((currLine[i] != '/') and (currLine[i] != '\n')) :
             validLines.insert(len(validLines), currLine) #checked, works
             
-    for y in range(len(validLines)) :
+    for y in range(len(validLines)) : #read and map gates to corresponding gate delays
         wordsInLine = validLines[y].split() 
         try:
             circuit.nodes[wordsInLine[0]].reqDelay = wordsInLine[1]
         except KeyError:
             pass
         
-
-        
 #calculation for part A =====================================================================
-
-#recursive function to calculate delays
-
-def calcDelayNode(circuit, node) -> float :
+def calcDelayNode(circuit, node) -> float :#recursive function to calculate delays
     maxDelay = 0
     gateDelay = circuit.gates[node.gate].gateDelay
     if(node.is_input == True):
@@ -219,11 +183,9 @@ def calcA(circuit):
     for outNode in circuit.outputNodesList :
         calcDelayNode(circuit, outNode)
         
-#for part B =======================================================================================
-
+#calculation for part B =======================================================================================
 def calcOutDelayNode(circuit, node) -> float :
     reqDelay = float('inf')
-    print(reqDelay)
     if(node.is_output == True):
         return node.reqDelay
     if(node.reqDelay != 0): #has been calculated earlier
@@ -241,19 +203,23 @@ def calcB(circuit):
     for outNode in circuit.inputNodesList :
         calcOutDelayNode(circuit, outNode)
 
-#writing outputs==========================================================
-            
-# def writeOutputDelayFile(circuit, filename) :
-#     for x in circuit.outputNodes:
-#         filename.write(str(x) + " " + str(circuit.outputNodesDelays[x]) + "\n" )
-    
-# close all files at the end!
+#writing outputs==========================================================            
+def writeOutputDelayFile(circuit, filename) :
+    for x in circuit.outputNodesList: #x is an output node object
+        delay = x.inDelay
+        if (int(delay) == delay):
+            delay = int(delay)
+        filename.write(str(x.nodeName) + " " + str(delay) + "\n" )
+        
+def writeInputDelayFile(circuit, filename) :
+    for x in circuit.inputNodesList:
+        delay = x.reqDelay
+        if (int(delay) == delay):
+            delay = int(delay)
+        filename.write(str(x.nodeName) + " " + str(delay) + "\n" )
 
 #main=======================================================================
-
-def main(): # checked, works
-    
-    print("Hello")
+def main():
     
     #all input files
     circuitFile = open('circuit.txt', 'r')
@@ -265,29 +231,22 @@ def main(): # checked, works
     inputDelays = open("input_delays.txt","a") # output for part B
     
     C = Circuit()
-    C.gates.append(Gate(0, 'dummy', 'dummy', 'dummy'))
-    # C.defineInputNode("A")
-    # C.defineInputNode("B")
-    # C.printInputNodes()
+    C.gates.append(Gate(0, 'dummy', 'dummy', 'dummy')) #dummy gate attached
 
     readCircuitFile(C, circuitFile) #tested, works
     readGateDelayFile (C, delayFile)
     readReqDelayFile (C, reqDelays)
-    C.displayCircuit()
-    # print(C.nodes['A'] < C.nodes['F'])
-    # C.nodesArray = C.inputNodesList + C.intNodesList + C.outputNodesList #works
-    # C.nodesArray.reverse() #works
-    # for i in range(len(C.nodesArray)):
-    #     print(C.nodesArray[i].nodeName)
-    # C.nodesArray.sort()
-    # # hq.heapify(C.nodesArray)
-    # for i in range(len(C.nodesArray)):
-    #     print(C.nodesArray[i].nodeName)
     calcA(C)
-    C.displayCircuit()
     calcB(C)
     C.displayCircuit()
-    # writeOutputDelayFile(C, outputDelayFile)
+    writeOutputDelayFile(C, outputDelayFile)
+    writeInputDelayFile(C, inputDelays)
+    
+    circuitFile.close()
+    delayFile.close()
+    reqDelays.close()
+    outputDelayFile.close()
+    inputDelays.close()
     
 if __name__ == "__main__":
     main()
--- sw1/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-def __lt__(self, otherNode):
-    #     return self.yos < nxt.yos
