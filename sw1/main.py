955a8c04d8cc4a1e39ce27dd65639b008889d971
loadthecode0 <artyaastha@gmail.com>
2023-08-21 13:57:56 +0530

    clean up

--- sw1/main.py
+++ sw1/main.py
@@ -1,27 +1,29 @@
 import sys
 
+#note: mapping input list to gates is not required, delete if you want later
+
 #defining node, gate and circuit objects  ========================================================================
 class Node :
     def __init__(self, index, name, if_input, if_output) -> None:
         self.nodeIndex = index
-        self.nodeName = name
+        self.nodeName = name #str
         self.is_input = if_input
         self.is_output = if_output
         self.gate = 0 # gate outputting to this node
         self.outGates = []
-        self.inputs = [] 
+        self.inputs = []  #list of nodes giving input to the gate associated with this node
         self.inDelay = 0 #for part A
         self.reqDelay = 0 #for Part B
         
     def displayNode(self):
-        print(self.nodeIndex, self.nodeName, self.is_input, self.is_output, self.gate, self.outGates, self.inDelay, self.inputs, self.reqDelay)
+        print(self.nodeIndex, self.nodeName, self.is_input, self.is_output, self.inputs, self.gate, self.outGates, self.inDelay, self.reqDelay)
         
 class Gate :
     def __init__(self, index, type, inputs, output) -> None:
         self.gateIndex = index
-        self.gateType = type
+        self.gateType = type #string
         self.gateDelay = 0 #delay at time of output
-        self.inputs = inputs #list of nodes giving input
+        self.inputs = inputs #list of nodes giving input to this gate
         self.output = output #Node taking output (str)
             
     def displayGate(self):
@@ -34,7 +36,7 @@ class Circuit:
         self.gateCount = 0 
         self.nodeCount = 0
                 
-        #dictionary of node objects, accessed by their names
+        #dictionary of node objects, accessed by their names as keys
         self.nodes = {}
         
         #lists of node objects
@@ -45,7 +47,7 @@ class Circuit:
         self.gates= [] #list containing all gate OBJECTS
         self.gateTypeDict = {}
         
-    def nodeInserter(self,arr, if_input, if_output) :
+    def nodeInserter(self,arr, if_input, if_output) : #helper function
         for z in range(len(arr)) :
             self.nodeCount += 1
             index = self.nodeCount
@@ -58,116 +60,79 @@ class Circuit:
                 self.outputNodesList.append(self.nodes[name])
             
     def displayCircuit(self) :
+        print(self.nodeCount, self.gateCount)
         for i in self.nodes:
             self.nodes[i].displayNode()
         for j in range(len(self.gates)) :
             self.gates[j].displayGate()
             
-
 #reading inputs and updating created objects ============================================================
 
-def readCircuitFile(circuit, filename) :
+def generateValidLinesList(filename) -> list:
     validLines = [] #storing valid input lines
     Lines = filename.readlines() # stores each line of circuit file as a string in a list Lines
-    
-    for x in range(len(Lines)) :
-        currLine = Lines[x]
+    for currLine in Lines :
         i = 0
         while (currLine[i] == ' ') : #find first character after any spaces
             i += 1
-        if ((currLine[i] != '/') and (currLine[i] != '\n')) :
+        if currLine[i] not in ['/', '\n'] : #then it is a valid line
             validLines.insert(len(validLines), currLine) #checked, works
-    
-    for y in range(len(validLines)) :
-        wordsInLine = validLines[y].split() 
+    return validLines
+
+def readCircuitFile(circuit, filename) :
+    validLines = generateValidLinesList(filename)    
+    for currLine in validLines :
+        wordsInLine = currLine.split() #list of all words in current line
         if (wordsInLine[0]=='PRIMARY_INPUTS' ):
-            circuit.inputNodesDelays = dict.fromkeys(wordsInLine[1:], 0)
-            circuit.nodeInserter(wordsInLine[1:], True, False) #inserts all nodes in current line
+            circuit.nodeInserter(wordsInLine[1:], True, False) #inserts all nodes in current line into the nodes List
         elif (wordsInLine[0]=='PRIMARY_OUTPUTS'):
-            circuit.outputNodesDelays = dict.fromkeys(wordsInLine[1:], 0)
             circuit.nodeInserter(wordsInLine[1:], False, True)
-            
         elif (wordsInLine[0]=='INTERNAL_SIGNALS'):
-            circuit.intNodesDelays = dict.fromkeys(wordsInLine[1:], 0)
             circuit.nodeInserter(wordsInLine[1:], False, False)
-        else :
+        else : #gates input now
             circuit.gateCount += 1
             index = circuit.gateCount
-            type = wordsInLine[0]
-            inputList = wordsInLine[1:(len(wordsInLine)-1)]
+            gateType = wordsInLine[0]
+            inputList = wordsInLine[1:-1]
             output = wordsInLine[len(wordsInLine)-1] #node to which this gate outputs to
-            circuit.gates.append(Gate(index, type, inputList, output))
+            circuit.gates.append(Gate(index, gateType, inputList, output))
             #assuming each internal or output node is associated as an output of a gate 
             #map each int/output node to the corresponding gate outputting to it
             circuit.nodes[output].gate = index
+            circuit.nodes[output].inputs = inputList
             #map each node inputting to this gate with the gate
             for nodeName in inputList:
                 if(circuit.nodes[nodeName].is_output == False):
                     circuit.nodes[nodeName].outGates.append(index)
+                else :
+                    raise Exception('Loop present in circuit: An output node cannot serve as input or internal signal')
             # circuit.gates[index-1].displayGate()    
-            try :
-                circuit.gateTypeDict[type].append(index)
+            try : # inserting each gate type into the dict (initialization)
+                circuit.gateTypeDict[gateType].append(index)
             except KeyError:
-                circuit.gateTypeDict[type] = []
-                circuit.gateTypeDict[type].append(index)
-    
-    #specifying input side nodes for each node
-    for i in circuit.nodes :
-        if(circuit.nodes[i].is_input == False):
-            gateNo = circuit.nodes[i].gate #list gates(s) outputting to this node
-            circuit.nodes[i].inputs = circuit.gates[gateNo].inputs
-            
-    #specifying input side nodes for each node
-    for i in circuit.nodes :
-        if(circuit.nodes[i].is_output == False):
-            gateNo = circuit.nodes[i].gate #list gates(s) outputting to this node
-            circuit.nodes[i].inputs = circuit.gates[gateNo].inputs
+                circuit.gateTypeDict[gateType] = [index]
     
 def readGateDelayFile (circuit, filename) :
-    
-    validLines = [] #storing valid input lines
-    Lines = filename.readlines() # stores each line of circuit file as a string in a list Lines
-    
-    for x in range(len(Lines)) :
-        currLine = Lines[x]
-        i = 0
-        while (currLine[i] == ' ') : #find first character after any spaces
-            i += 1
-        if ((currLine[i] != '/') and (currLine[i] != '\n')) :
-            validLines.insert(len(validLines), currLine) #checked, works
-            
-    for y in range(len(validLines)) :
-        wordsInLine = validLines[y].split() 
-        type = wordsInLine[0]
+    validLines = generateValidLinesList(filename)    
+    for currLine in validLines :
+        wordsInLine = currLine.split()
+        gateType = wordsInLine[0]
         try:
             delay = wordsInLine[1]
-            gateIndices = circuit.gateTypeDict[type]
-            for z in range(len(gateIndices)) :
-                gate = circuit.gates[gateIndices[z]]
-                gate.gateDelay = delay
-                # gate.displayGate()
+            gateIndices = circuit.gateTypeDict[gateType]
+            for index in gateIndices :
+                circuit.gates[index].gateDelay = delay
         except KeyError:
-            pass
+            print('Gate not present in circuit')
         
 def readReqDelayFile (circuit, filename) :
-    
-    validLines = [] #storing valid input lines
-    Lines = filename.readlines() # stores each line of circuit file as a string in a list Lines
-    
-    for x in range(len(Lines)) :
-        currLine = Lines[x]
-        i = 0
-        while (currLine[i] == ' ') : #find first character after any spaces
-            i += 1
-        if ((currLine[i] != '/') and (currLine[i] != '\n')) :
-            validLines.insert(len(validLines), currLine) #checked, works
-            
-    for y in range(len(validLines)) : #read and map gates to corresponding gate delays
-        wordsInLine = validLines[y].split() 
+    validLines = generateValidLinesList(filename)                
+    for currLine in validLines :
+        wordsInLine = currLine.split()
         try:
             circuit.nodes[wordsInLine[0]].reqDelay = wordsInLine[1]
         except KeyError:
-            pass
+            print('Node not present in circuit')
         
 #calculation for part A =====================================================================
 def calcDelayNode(circuit, node) -> float :#recursive function to calculate delays
@@ -206,52 +171,44 @@ def calcB(circuit):
         calcOutDelayNode(circuit, outNode)
 
 #writing outputs==========================================================            
-def writeOutputDelayFile(circuit, filename) :
+def writeOutputDelayFile(circuit, filename):
     for x in circuit.outputNodesList: #x is an output node object
         delay = x.inDelay
         if (int(delay) == delay):
             delay = int(delay)
-        filename.write(str(x.nodeName) + " " + str(delay) + "\n" )
+        filename.write(f"{str(x.nodeName)} {str(delay)}" + "\n")
         
 def writeInputDelayFile(circuit, filename) :
     for x in circuit.inputNodesList:
         delay = x.reqDelay
         if (int(delay) == delay):
             delay = int(delay)
-        filename.write(str(x.nodeName) + " " + str(delay) + "\n" )
+        filename.write(f"{str(x.nodeName)} {str(delay)}" + "\n")
 
 #main=======================================================================
-def main():
+def main():  
+    C = Circuit() #declare circuit
+    C.gates.append(Gate(0, 'dummy', 'dummy', 'dummy')) #dummy gate attached
     
     #all input files
-    circuitFile = open(sys.argv[2], 'r')
-    delayFile = open(sys.argv[3],'r') #universal : gate delay values for each gate
-    reqDelays = open(sys.argv[4], 'r') # input for part B
-    
-    #all output files
-    outputDelayFile = open("output_delays.txt","a") # output for part A
-    inputDelays = open("input_delays.txt","a") # output for part B
-    
-    C = Circuit()
-    C.gates.append(Gate(0, 'dummy', 'dummy', 'dummy')) #dummy gate attached
+    with open(sys.argv[2], 'r') as circuitFile: # input for part A
+        readCircuitFile(C, circuitFile) #tested, works
+    with open(sys.argv[3],'r') as delayFile: #universal : gate delay values for each gate
+        readGateDelayFile (C, delayFile)
+    with open(sys.argv[4], 'r') as reqDelays: # input for part B
+        readReqDelayFile (C, reqDelays)
 
-    readCircuitFile(C, circuitFile) #tested, works
-    readGateDelayFile (C, delayFile)
-    readReqDelayFile (C, reqDelays)
-    
+    #all output files
     if sys.argv[1] == 'A' :
         calcA(C)
-        writeOutputDelayFile(C, outputDelayFile)
+        with open("output_delays.txt","a") as outputDelayFile:
+            writeOutputDelayFile(C, outputDelayFile) # output for part A
     elif sys.argv[1] == 'B':
         calcB(C)
-        writeInputDelayFile(C, inputDelays)
+        with open("input_delays.txt","a") as inputDelays:
+            writeInputDelayFile(C, inputDelays)# output for part B
     
     C.displayCircuit()
-    circuitFile.close()
-    delayFile.close()
-    reqDelays.close()
-    outputDelayFile.close()
-    inputDelays.close()
     
 if __name__ == "__main__":
     main()
